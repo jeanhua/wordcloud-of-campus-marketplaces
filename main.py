0139d6a8afa4609d6765c5bf51d05d80240de3f6
jeanhua <2207739460@qq.com>
2024-06-17 22:41:19 +0800

    多线程支持

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import json
+import os
 import sys
-
+import threading
 import requests
 import drawMap
 
 
-class getContent:
+class getContent_thread:
+    __threadlist = []
+    already = 0
     def __init__(self, nums: int):
         self.nums = nums
         self.__getNewestId()
@@ -18,7 +21,7 @@ class getContent:
             print('无法获取最新帖子ID')
             sys.exit(0)
 
-    def getmsg(self,id):
+    def getmsg(self, id):
         # 发送http请求
         url = 'https://u.xiaouni.com/user-api/content/article/info?id=' + str(id)
         response = requests.get(url=url)
@@ -34,19 +37,41 @@ class getContent:
         else:
             return 'none'
 
+
+
     def run(self):
-        for i in range(1, self.nums+1):
+        with open('content.txt', 'a', encoding='utf-8') as f:
+            if self.nums <= 8:
+                for m_thread in range(0, self.nums):
+                    t = threading.Thread(target=self.__run, args=(f, m_thread, m_thread))
+                    self.__threadlist.append(t)
+                    t.start()
+            else:
+                stringNum: int = int(self.nums / 8)
+                for m_thread in range(0, 8):
+                    t = threading.Thread(target=self.__run,
+                                         args=(f, m_thread * stringNum, m_thread * stringNum + stringNum))
+                    self.__threadlist.append(t)
+                    t.start()
+                if stringNum * 8 < self.nums:
+                    t = threading.Thread(target=self.__run, args=(f, 8 * stringNum, self.nums - stringNum * 8))
+                    self.__threadlist.append(t)
+                    t.start()
+            for it in self.__threadlist:
+                it.join()
+
+    def __run(self, f, begin, end):
+        for i in range(begin, end):
             id = self.newestId - i
-            print(i)
             text = self.getmsg(id)
+            print(text[0:20],'……')
             if text != 'none':
-                with open('content.txt', 'a', encoding='utf-8') as f:
-                    f.write(text)
+                f.write(text)
 
 
 if __name__ == '__main__':
     num = int(input("输入要获取的帖子数目(建议300条左右)："))
-    getContent(num).run()
+    getContent_thread(num).run()
     print('已完成帖子获取,正在生成词云图……')
     drawMap.draw('wordcloud.png')
     print('词云生成完毕！')
