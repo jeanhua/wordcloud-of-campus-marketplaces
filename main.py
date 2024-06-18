af21f20665a7013e592a81c3d11a63ec7678fb84
jeanhua <2207739460@qq.com>
2024-06-18 11:21:43 +0800

    多线程支持

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import drawMap
 class getContent_thread:
     __threadlist = []
     already = 0
+
     def __init__(self, nums: int):
         self.nums = nums
         self.__getNewestId()
@@ -37,36 +38,51 @@ class getContent_thread:
         else:
             return 'none'
 
-
-
     def run(self):
-        with open('content.txt', 'a', encoding='utf-8') as f:
-            if self.nums <= 8:
-                for m_thread in range(0, self.nums):
-                    t = threading.Thread(target=self.__run, args=(f, m_thread, m_thread))
-                    self.__threadlist.append(t)
-                    t.start()
-            else:
-                stringNum: int = int(self.nums / 8)
-                for m_thread in range(0, 8):
-                    t = threading.Thread(target=self.__run,
-                                         args=(f, m_thread * stringNum, m_thread * stringNum + stringNum))
-                    self.__threadlist.append(t)
-                    t.start()
-                if stringNum * 8 < self.nums:
-                    t = threading.Thread(target=self.__run, args=(f, 8 * stringNum, self.nums - stringNum * 8))
-                    self.__threadlist.append(t)
-                    t.start()
-            for it in self.__threadlist:
-                it.join()
+        if self.nums <= 8:
+            for m_thread in range(0, self.nums):
+                t = threading.Thread(target=self.__run, args=(m_thread, m_thread, self.nums))
+                self.__threadlist.append(t)
+                t.start()
+        else:
+            stringNum: int = int(self.nums / 8)
+            for m_thread in range(0, 8):
+                t = threading.Thread(target=self.__run,
+                                     args=(m_thread, m_thread * stringNum, m_thread * stringNum + stringNum))
+                self.__threadlist.append(t)
+                t.start()
+            if stringNum * 8 < self.nums:
+                t = threading.Thread(target=self.__run, args=(8, 8 * stringNum, self.nums - stringNum * 8))
+                self.__threadlist.append(t)
+                t.start()
+        for it in self.__threadlist:
+            it.join()
+        content_txt = ''
+        for i in range(0, 9):
+            with open(str(i) + '.txt', 'r', encoding='utf-8') as f:
+                try:
+                    content_txt += f.read()
+                    f.close()
+                except IOError as e:
+                    print(str(i) + ':', e)
+        with open('content.txt', 'w', encoding='utf-8') as f:
+            f.write(content_txt)
+            f.close()
+            for i in range(0, 9):
+                try:
+                    os.remove(str(i) + '.txt')
+                except IOError as e:
+                    print(str(i) + '(remove):', e)
 
-    def __run(self, f, begin, end):
-        for i in range(begin, end):
-            id = self.newestId - i
-            text = self.getmsg(id)
-            print(text[0:20],'……')
-            if text != 'none':
-                f.write(text)
+    def __run(self, thread_id, begin, end):
+        with open(str(thread_id) + ".txt", 'a', encoding='utf-8') as f:
+            for i in range(begin, end):
+                id = self.newestId - i
+                text = self.getmsg(id)
+                print(text[0:20], '……')
+                if text != 'none':
+                    f.write(text)
+            f.close()
 
 
 if __name__ == '__main__':
